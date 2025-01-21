b807172da49347b8b18316dae3425f1b7c36f0e6
Lina <g.korotkova@lucky-team.pro>
2025-01-21 12:57:31 +0300

    create build

--- src/store/root-reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-//import { stepReducer } from "./step/step-reducer";
-//import { formReducer } from "./form/form-reducer";
-
-//export const rootReducer = combineReducers({
-//  step: stepReducer,
-//  form: formReducer,
-//});
